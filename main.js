e0c53c2fb7db6121143f19d6c5641eb1537407ff
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-02-01 13:27:05 +0530

    file created

---
+++ main.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const mongoose=require("mongoose");
+
+
+const app=express();
+
+const cors=require("cors");
+const dotenv=require("dotenv");
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+let corsOptions = {
+    origin:["http://localhost:4000"],
+};
+PORT = 4000;
+
+db_Url = "mongodb://localhost:27017/forgotpassword-bc";
+
+mongoose
+.connect(db_Url)
+.then(console.log("db connected"))
+.catch((err) => {
+    console.log("db not connected");
+});
+
+app.listen(PORT,() => {
+    console.log("server start on port 4000");
+});
+
+
+
